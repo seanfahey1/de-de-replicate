bc683dac30577a20d3ed72a50fc860b3e116d1af
Sean <sfahey1745@sdsu.edu>
2024-02-03 12:39:25 -0800

    ''

---
+++ de-de-replicate.sh
@@ -0,0 +1,20 @@
+files="./*.fasta"
+
+# remove anything w/in a class that's 100% identical, keeps the longer seq.
+for file in $files;
+do
+  echo "$file"
+  file_clean=$(basename "$file" ".fasta")
+
+  # write the filename into the header so we can separate them later
+  sed -i "s/^>/>$file_clean /g" "$file" > "$file_clean"_named.fasta
+
+  cd-hit -c 1 -T 0 -M 0 -i "$file" -o "$file_clean"_100_self.fasta
+  cat "$file_clean"_100_self.fasta >> all_files.fasta
+
+done
+
+# remove anything that has 100% similarity with something in another class. then unmerge files.
+cd-hit -c 1 -T 0 -M 0 -i all_files.fasta -o all_files_100.fasta
+python3 unmerge.py -f all_files_100.fasta -c all_files_100.fasta.clstr
+
---
+++ unmerge_100.py
@@ -0,0 +1,48 @@
+import argparse
+import re
+import sys
+
+from Bio import SeqIO
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Rebuild original fasta files from a merged file using first word of header of each seq',
+        epilog='This is meant to be run in the de-de-replicate pipeline')
+
+    parser.add_argument('-f', '--fasta', required=True)
+    parser.add_argument('-c', '--clstr', required=True)
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = get_args()
+    fasta, clstr = args['fasta'], args['clstr']
+    drop_headers = []
+
+    with open(clstr, 'r') as c:
+        c = c.read().split(">Cluster")[1:]
+        for cluster in c:
+            seq_count = len([x for x in cluster.split('\n') if re.match(r'[0-9]{1,4}/s+')])
+            if seq_count > 1:
+                headers = re.findall(r'[0-9]+/s+(>.*)$')
+                for header in headers:
+                    drop_headers.append(header)
+
+    for record in SeqIO.parse(fasta, "fasta"):
+        full_header = record.description.split(' ')
+        header = ' '.join(full_header[1:])
+        group = full_header[0]
+
+        sequence = str(record.seq)
+        if header not in drop_headers:
+            with open(f'{group}_100.fasta', 'a') as out:
+                out.write(f">{header}")
+                out.write(sequence)
+                out.write('\n')
+
+
+if __name__ == "__main__":
+    sys.exit(main())
