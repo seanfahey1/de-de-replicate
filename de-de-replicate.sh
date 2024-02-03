f8d5e40aea82023b4784715f6c7fc35cc4e19c25
Sean <sfahey1745@sdsu.edu>
2024-02-03 14:00:34 -0800

    'sed was creating empty files'

--- de-de-replicate.sh
+++ de-de-replicate.sh
@@ -9,7 +9,8 @@ do
   file_clean=$(basename "$file" ".fasta")
 
   # write the filename into the header so we can separate them later
-  sed -i "s/^>/>$file_clean /g" "$file" > "$file_clean"_named.fasta
+  cp "$file" "$file_clean"_named.fasta
+  sed -i "s/^>/>$file_clean /g" "$file_clean"_named.fasta
 
   cd-hit -c 1 -T 0 -M 0 -d 500 -i "$file_clean"_named.fasta -o "$file_clean"_100_self.fasta
   cat "$file_clean"_100_self.fasta >> all_files_100_self.fasta
