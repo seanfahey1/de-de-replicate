ddb6d0ec366b52b2ebf3b8ac50ed68b1c51be638
Sean <sfahey1745@sdsu.edu>
2024-02-22 13:03:22 -0800

    'skipping cd-hit for rerun'

--- de-de-replicate.sh
+++ de-de-replicate.sh
@@ -13,10 +13,10 @@ for file in "$target_dir"/*_hashed_descriptors.fasta; do
   output="$dirname/$filename_no_ext"_40.fasta
   clstr_output="$output".clstr
 
-  echo "  will output to $output"
-
-  echo "  running CD-Hit"
-  cd-hit -c 0.4 -n 2 -T 0 -M 0 -sc 1 -d 0 -i "$file" -o "$output"
+#  echo "  will output to $output"
+#
+#  echo "  running CD-Hit"
+#  cd-hit -c 0.4 -n 2 -T 0 -M 0 -sc 1 -d 0 -i "$file" -o "$output"
 
   echo "  running split-expand step"
   ./split_expand.py --f "$file" --c "$clstr_output" --n 11 --o "$target_dir"/groups/
