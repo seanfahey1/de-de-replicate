8fcfa4ebf7a22714dac0e13e49a4f2fd56e1bcbe
Sean <sfahey1745@sdsu.edu>
2024-02-21 22:25:14 -0800

    ''

--- de-de-replicate.sh
+++ de-de-replicate.sh
@@ -16,7 +16,7 @@ for file in "$target_dir"/*_hashed_descriptors.fasta; do
   echo "  will output to $output"
 
   echo "  running CD-Hit"
-  cd-hit -c 0.4 -n 2 -T 0 -M 0 -sc 1 -d 100 -i "$file" -o "$output"
+  cd-hit -c 0.4 -n 2 -T 0 -M 0 -sc 1 -d 0 -i "$file" -o "$output"
 
   echo "  running split-expand step"
   ./split_expand.py --f "$file" --c "$clstr_output" --n 11 --o "$target_dir"/groups/
