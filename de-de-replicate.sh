65701a4f806e4ab4fe80eb9ac32e2adea0a4c349
Sean <sfahey1745@sdsu.edu>
2024-02-21 18:11:19 -0800

    'cluster line length'

--- de-de-replicate.sh
+++ de-de-replicate.sh
@@ -9,7 +9,7 @@ for file in "$target_dir"/*.fasta; do
   clstr_output="$output".clstr
 
   echo "  running CD-Hit"
-  cd-hit -c 0.9 -n 2 -T 0 -M 0 -sc -d 100 -i "$file" -o "$output" > /dev/null
+  cd-hit -c 0.4 -n 2 -T 0 -M 0 -sc -d 150 -i "$file" -o "$output" > /dev/null
 
   echo "  running split-expand step"
   ./split_expand.py --f "$file" --c "$clstr_output" --n 11 --o "$target_dir"/groups/
