d616568a9db05c2fb9f2b47b9341afcf8f385ceb
Sean <sfahey1745@sdsu.edu>
2024-02-21 21:10:27 -0800

    'no output display for cdhit'

--- de-de-replicate.sh
+++ de-de-replicate.sh
@@ -2,7 +2,7 @@
 
 target_dir="$1"
 
-for file in "$target_dir"*.fasta; do
+for file in "$target_dir"/*.fasta; do
   echo "starting $file..."
 
   dirname=$(dirname "$file")
@@ -14,7 +14,7 @@ for file in "$target_dir"*.fasta; do
   echo "  will output to $output"
 
   echo "  running CD-Hit"
-  cd-hit -c 0.4 -n 2 -T 0 -M 0 -sc 1 -d 150 -i "$file" -o "$output"
+  cd-hit -c 0.4 -n 2 -T 0 -M 0 -sc 1 -d 150 -i "$file" -o "$output" > /dev/null
 
   echo "  running split-expand step"
   ./split_expand.py --f "$file" --c "$clstr_output" --n 11 --o "$target_dir"/groups/
